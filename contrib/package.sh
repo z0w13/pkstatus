c9371065d771895859e99108fdf2ac3634c63a1c
Zowie <git@zowie.cx>
2024-02-25 22:15:29 +0100

    build: strip any suffixed from BASE_VERSION during dev build

--- contrib/package.sh
+++ contrib/package.sh
@@ -9,7 +9,7 @@ BASE_VERSION="$(jq -r ".version" package.json)"
 if [[ "${NODE_ENV:=}" == "production" ]]; then
   PKG_VERSION="${BASE_VERSION}"
 else
-  PKG_VERSION="${BASE_VERSION}-dev+$(git rev-parse --short HEAD)"
+  PKG_VERSION="${BASE_VERSION%%-*}-dev+$(git rev-parse --short HEAD)"
 fi
 
 # Override version for packaging
