802c26664dbc42ce9f334d0e2c27319b8aeb4642
Zowie <git@zowie.cx>
2024-02-25 21:58:45 +0100

    feat(ui): rework last switch/last update ui

--- src/components/StatusPage/Tile/SystemView.vue
+++ src/components/StatusPage/Tile/SystemView.vue
@@ -10,6 +10,38 @@
         {{ system.name }}
       </div>
 
+      <q-card-section v-if="fronters && showStats">
+        <q-item v-if="showUpdateTime">
+          <q-item-section avatar>
+            <q-icon name="update" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label> Last updated </q-item-label>
+            <q-item-label caption>
+              <relative-time-display :time="fronters.lastUpdated" />
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item v-if="showLastSwitch">
+          <q-item-section avatar>
+            <q-icon name="swap_horiz" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label> Last switch </q-item-label>
+            <q-item-label caption>
+              <relative-time-display
+                v-if="!!fronters.lastSwitch"
+                :time="fronters.lastSwitch"
+              />
+              <template v-else>unknown</template>
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-card-section>
+
+      <q-separator v-if="showDescription && showStats" />
+
       <q-card-section v-if="showDescription">
         <span v-html="system.description.replaceAll('\n', '<br />')" />
       </q-card-section>
@@ -67,12 +99,15 @@
 import { Fronters } from 'stores/fronters-store';
 import { System } from 'src/models/System';
 import AlterView from 'src/components/StatusPage/Tile/AlterView.vue';
+import RelativeTimeDisplay from 'src/components/RelativeTimeDisplay.vue';
 import { computed } from 'vue';
 
 export interface Props {
   system: System;
   fronters?: Fronters;
 
+  showLastSwitch: boolean;
+  showUpdateTime: boolean;
   showSystemDescription: boolean;
   showFronterDescription: boolean;
   cardWidth: number;
@@ -82,4 +117,5 @@ const props = defineProps<Props>();
 const showDescription = computed(
   () => props.showSystemDescription && props.system.description.length > 0,
 );
+const showStats = computed(() => props.showLastSwitch || props.showUpdateTime);
 </script>
--- src/pages/status/TileLayout.vue
+++ src/pages/status/TileLayout.vue
@@ -1,37 +1,14 @@
 <template>
   <div class="container col-10">
-    <div class="row justify-left q-pa-md">
-      <div class="col-auto"></div>
-    </div>
     <template :key="id" v-for="[id, system] in Object.entries(systems)">
-      <div
-        class="row justify-left"
-        v-if="settings.showUpdateTime && fronters[id]"
-      >
-        <div class="col-auto text-italic">
-          Last updated
-          <relative-time-display :time="fronters[id].lastUpdated" />
-        </div>
-      </div>
-      <div
-        class="row justify-left"
-        v-if="settings.showLastSwitch && fronters[id]"
-      >
-        <div class="col-auto text-italic">
-          Last switch
-          <relative-time-display
-            v-if="!!fronters[id].lastSwitch"
-            :time="fronters[id].lastSwitch"
-          />
-          <template v-else>unknown</template>
-        </div>
-      </div>
       <div class="row justify-left q-pa-md q-col-gutter-md">
         <system-view
           :system="system"
           :fronters="fronters[id]"
           :show-system-description="settings.showSystemDescription"
           :show-fronter-description="settings.showFronterDescription"
+          :show-update-time="settings.showUpdateTime"
+          :show-last-switch="settings.showLastSwitch"
           :card-width="settings.tileSize"
         />
       </div>
@@ -45,7 +22,6 @@ import { System } from 'src/models/System';
 import { useSettingsStore } from 'src/stores/settings-store';
 
 import SystemView from 'src/components/StatusPage/Tile/SystemView.vue';
-import RelativeTimeDisplay from 'src/components/RelativeTimeDisplay.vue';
 
 const settings = useSettingsStore().status.tile;
 
