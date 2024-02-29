55a3b4cf3d5caf9bd8361b97d62780bd28e53bfe
Zowie <git@zowie.cx>
2024-02-29 16:28:53 +0100

    feat(status/table): add mobile view

---
+++ src/pages/status/Table/TableEntity.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-item dense class="q-pa-none">
+    <q-item-section avatar class="col-auto q-pr-sm">
+      <initial-fallback-avatar
+        v-if="!icon"
+        :name="text"
+        :url="img"
+        :size="size"
+        :square="square"
+        :color="color"
+      />
+      <q-avatar
+        v-else
+        :color="color"
+        :icon="icon"
+        :size="size"
+        :square="square"
+      />
+    </q-item-section>
+    <q-item-section class="col-auto">
+      {{ text }}
+
+      <q-tooltip v-if="tooltip">
+        <pre class="description">tooltip</pre>
+      </q-tooltip>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import InitialFallbackAvatar from 'src/components/InitialFallbackAvatar.vue';
+
+withDefaults(
+  defineProps<{
+    tooltip?: string | null;
+    text: string;
+    icon?: string | null;
+    color?: string;
+    img?: string | null;
+    size: string;
+    square: boolean;
+  }>(),
+  {
+    color: 'primary',
+  },
+);
+</script>
--- src/pages/status/TableLayout.vue
+++ src/pages/status/TableLayout.vue
@@ -6,109 +6,96 @@
     <div :class="['col', 'col-lg-auto', `self-${settings.verticalPosition}`]">
       <q-markup-table flat>
         <thead>
-          <th>System</th>
-          <th :colspan="maxFront + 1" style="width: 100%">Fronters</th>
-          <th v-if="settings.showLastSwitch">Last Switch</th>
-          <th v-if="settings.showUpdateTime">Last Update</th>
+          <tr :style="`line-height: ${settings.iconSize * 1.1}px`">
+            <th>System</th>
+            <th
+              :colspan="useMobileUi ? 1 : maxFront + 1"
+              :style="{ width: useMobileUi ? 'auto' : '100%' }"
+            >
+              Fronters
+            </th>
+            <th v-if="settings.showLastSwitch">Last Switch</th>
+            <th v-if="settings.showUpdateTime">Last Update</th>
+          </tr>
         </thead>
         <tbody>
-          <tr :key="id" v-for="[id, system] in Object.entries(systems)">
-            <td>
-              <q-avatar
-                v-if="system.avatarUrl"
-                square
+          <tr
+            :key="id"
+            v-for="[id, system] in Object.entries(systems)"
+            :style="`line-height: ${settings.iconSize * 1.1}px`"
+          >
+            <td valign="top">
+              <table-entity
+                :img="system.avatarUrl"
                 :size="settings.iconSize + 'px'"
-              >
-                <q-img :ratio="1" :src="system.avatarUrl">
-                  <template v-slot:error>
-                    <q-icon
-                      :size="`${settings.iconSize}px`"
-                      class="absolute-center"
-                      color="grey"
-                      name="broken_image"
-                    />
-                  </template>
-                </q-img>
-              </q-avatar>
-              <q-avatar
-                v-else
+                :tooltip="system.description"
+                :text="system.name"
                 square
-                color="primary"
-                icon="groups"
-                :size="`${settings.iconSize}px`"
               />
-              {{ system.name }}
-              <q-tooltip v-if="system.description">
-                <pre class="description">{{ system.description }}</pre>
-              </q-tooltip>
             </td>
             <template v-if="fronters[id]">
               <template v-if="fronters[id].allowed">
-                <td
-                  style="width: 1%"
-                  :key="fronter.id"
-                  v-for="fronter of fronters[id].members"
-                >
-                  <q-avatar
-                    v-if="fronter.avatarUrl"
-                    square
+                <td v-if="useMobileUi" valign="top">
+                  <table-entity
+                    v-for="fronter of fronters[id].members"
+                    :key="fronter.id"
+                    :img="fronter.avatarUrl"
                     :size="settings.iconSize + 'px'"
-                  >
-                    <q-img :ratio="1" :src="fronter.avatarUrl">
-                      <template v-slot:error>
-                        <q-icon
-                          :size="`${settings.iconSize}px`"
-                          class="absolute-center"
-                          color="grey"
-                          name="broken_image"
-                        />
-                      </template>
-                    </q-img>
-                  </q-avatar>
-                  <q-avatar
-                    v-else
-                    color="primary"
-                    icon="person"
+                    :tooltip="fronter.description"
+                    :text="fronter.displayName || fronter.name"
                     square
-                    :size="settings.iconSize + 'px'"
                   />
-                  {{ fronter.displayName || fronter.name }}
-
-                  <q-tooltip v-if="fronter.description">
-                    <pre class="description">{{ fronter.description }}</pre>
-                  </q-tooltip>
                 </td>
-                <td
-                  v-if="maxFront - fronters[id].members.length > 0"
-                  :colspan="maxFront - fronters[id].members.length"
-                ></td>
+                <template v-else>
+                  <td
+                    valign="top"
+                    style="width: 1%"
+                    :key="fronter.id"
+                    v-for="fronter of fronters[id].members"
+                  >
+                    <table-entity
+                      :img="fronter.avatarUrl"
+                      :size="settings.iconSize + 'px'"
+                      :tooltip="fronter.description"
+                      :text="fronter.displayName || fronter.name"
+                      square
+                    />
+                  </td>
+                  <td
+                    v-if="maxFront - fronters[id].members.length > 0"
+                    :colspan="maxFront - fronters[id].members.length"
+                  ></td>
+                </template>
               </template>
               <!-- No Access -->
               <template v-else>
                 <td>
-                  <q-avatar
-                    color="red"
-                    icon="close"
-                    square
+                  <table-entity
+                    text="No Access"
                     :size="settings.iconSize + 'px'"
+                    icon="close"
+                    color="red"
+                    square
                   />
-                  No Access
                 </td>
-                <td v-if="maxFront > 1" :colspan="maxFront - 1"></td>
+                <td
+                  v-if="maxFront > 1 && !useMobileUi"
+                  :colspan="maxFront - 1"
+                ></td>
               </template>
             </template>
             <!-- Loading -->
-            <td v-else>
+            <td v-else valign="top">
               <q-linear-progress query />
             </td>
             <!-- Spacer -->
-            <td />
+            <td v-if="!useMobileUi" />
             <!-- Last Switch -->
-            <td v-if="settings.showLastSwitch && fronters[id]">
+            <td v-if="settings.showLastSwitch && fronters[id]" valign="top">
               <relative-time-display :time="fronters[id].lastSwitch" />
             </td>
             <!-- Last Updated -->
-            <td v-if="settings.showUpdateTime && fronters[id]">
+            <td v-if="settings.showUpdateTime && fronters[id]" valign="top">
               <relative-time-display :time="fronters[id].lastUpdated" />
             </td>
           </tr>
@@ -126,8 +113,13 @@ import { useSettingsStore } from 'src/stores/settings-store';
 import { System } from 'src/models/System';
 
 import RelativeTimeDisplay from 'src/components/RelativeTimeDisplay.vue';
+import TableEntity from 'src/pages/status/Table/TableEntity.vue';
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
 
 const settings = useSettingsStore().status.table;
+const useMobileUi = computed(() => $q.screen.lt.sm);
 
 export interface Props {
   fronters: Record<string, Fronters>;
