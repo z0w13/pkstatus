6609d3e940f668143c3ea2794bdb09469e5df496
Zowie <git@zowie.cx>
2024-03-12 20:35:38 +0100

    Generated v0.3.1

---
+++ assets/QCardSection-6m2oLwX-.js
@@ -0,0 +1,1 @@
+import{c as e,a as r,h as n,d as s}from"./index-CftRmOSb.js";const i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
