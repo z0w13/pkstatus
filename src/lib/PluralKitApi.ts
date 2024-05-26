18b89e9a416645299d0dca414aee470cfe4b8cf1
Zowie <git@zowie.cx>
2024-05-26 19:15:50 +0200

    feat(api): rework ratelimiting based on API response headers

--- src/lib/PluralKitApi.ts
+++ src/lib/PluralKitApi.ts
@@ -1,20 +1,26 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import PKAPI, { APIData, APIError } from 'pkapi.js';
-import Bottleneck from 'bottleneck';
+import dayjs from 'dayjs';
+
+interface RequestPath {
+  method: string;
+  route: string;
+}
+interface RequestOptions {
+  token?: string;
+  headers?: any;
+  body?: any;
+  priority?: number;
+}
 
 type HandleReturn = ReturnType<PKAPI['handle']>;
+type PKApiResponse = Awaited<HandleReturn>;
 
 export default class PluralKitApi extends PKAPI {
-  private limiter: Bottleneck;
   private requestQueues: Map<string, HandleReturn>;
 
   constructor(data?: APIData) {
     super(data);
-
-    this.limiter = new Bottleneck({
-      minTime: 1000,
-      maxConcurrent: 1,
-    });
 
     this.requestQueues = new Map<string, HandleReturn>();
   }
@@ -23,47 +29,20 @@ export default class PluralKitApi extends PKAPI {
     return token ? `${method} | ${route} | ${token}` : `${method} | ${route}`;
   }
 
-  async handle(
-    path: { method: string; route: string },
-    options?: {
-      token?: string;
-      headers?: any;
-      body?: any;
-    },
-  ): ReturnType<PKAPI['handle']> {
-    const key = this.queueKey(path.method, path.route, options?.token);
+  handle(path: RequestPath, options?: RequestOptions): HandleReturn {
+    // TODO: Look into prioritized queueing
+    const key = this.queueKey(path.method, path.route);
 
     // Only deduplicate get requests
-    if (path.method == 'GET') {
+    if (path.method == 'GET' && this.requestQueues.has(key)) {
       // Don't create new responses if we're already waiting for one
-      const promise = this.requestQueues.get(key);
-      if (promise) {
-        return promise;
-      }
+      console.debug('Returning existing request with key:', key);
+      return this.requestQueues.get(key)!;
     }
 
-    // eslint-disable-next-line no-async-promise-executor
-    const prom = new Promise<Awaited<HandleReturn>>(async (resolve, reject) => {
-      // eslint-disable-next-line no-constant-condition
-      while (true) {
-        try {
-          await this.limiter.schedule(async () => {
-            resolve(await super.handle(path, options));
-          });
-
-          // API didn't throw an error, so promise is resolved, remove it
-          break;
-        } catch (e) {
-          // 429 Too Many Requests
-          if (e instanceof APIError && e.status == '429') {
-            console.log('Too many requests, retrying...');
-          } else {
-            // Not an expected error reject the promise
-            return reject(e);
-          }
-        }
-      }
-    });
+    const prom = new Promise<Awaited<HandleReturn>>((resolve, reject) =>
+      this.innerHandle(resolve, reject, path, options),
+    );
 
     if (path.method == 'GET') {
       // Make sure to delete promise from cache when it resolves
@@ -71,5 +50,44 @@ export default class PluralKitApi extends PKAPI {
       prom.finally(() => this.requestQueues.delete(key));
     }
     return prom;
+  }
+
+  private async innerHandle(
+    resolve: (value: PKApiResponse) => void,
+    reject: (reason: unknown) => void,
+    path: RequestPath,
+    options?: RequestOptions,
+  ): Promise<void> {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      try {
+        const res = await super.handle(path, options);
+        await this.handleRatelimitHeaders(res);
+        return resolve(res);
+      } catch (e) {
+        if (e instanceof APIError && e.status == '429') {
+          // Hit the rate limit
+          await this.handleRatelimitHeaders(e);
+        } else {
+          return reject(e);
+        }
+      }
+    }
+  }
+
+  private async handleRatelimitHeaders(
+    res: PKApiResponse | APIError,
+  ): Promise<void> {
+    // TODO: Extra backoff if hitting ratelimit multiple times
+
+    // const limit = res.headers['x-ratelimit-limit'];
+    const remaining = res.headers['x-ratelimit-remaining'];
+    const reset = res.headers['x-ratelimit-reset'];
+    const resetSeconds = Math.max(reset - dayjs().unix(), 1);
+
+    if (remaining < 1) {
+      console.info(`Rate limit reached, waiting for ${resetSeconds} seconds`);
+      await new Promise((resolve) => setTimeout(resolve, resetSeconds * 1_000));
+    }
   }
 }
