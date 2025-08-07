99d46f6866601e2a827b5bd75ed4e93c793f44bb
zowie <git@zowie.cx>
2025-08-07 22:08:33 +0200

    fix(electron): don't auto-open devtools in debugging/dev builds

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -41,16 +41,6 @@ function createWindow() {
     mainWindow.loadFile('index.html');
   }
 
-  if (process.env.DEBUGGING) {
-    // if on DEV or Production with debug enabled
-    mainWindow.webContents.openDevTools();
-  } else {
-    // we're on production; no access to devtools pls
-    //mainWindow.webContents.on('devtools-opened', () => {
-    //  mainWindow?.webContents.closeDevTools();
-    //});
-  }
-
   mainWindow.on('closed', () => {
     mainWindow = undefined;
   });
